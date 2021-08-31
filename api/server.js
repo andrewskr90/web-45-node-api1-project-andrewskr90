664b7ebf7883d24d6c7d5b39f8e8918410da5749
andrewskr90 <andrewskr90@gmail.com>
2021-08-30 19:00:23 -0700

    1-3 testing correct

--- api/server.js
+++ api/server.js
@@ -1,3 +1,33 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const Users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+server.use(express.json())
+
+server.post('/api/users', (req, res) => {
+    Users.insert(req.body)
+        .then(user => {
+            if(user) {
+                res.status(201).json(user)
+            } else {
+                res.status(500).json({ message: "There was an error while saving the user to the database" })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(400).json({ message: "Please provide name and bio for the user" })
+        })
+})
+
+server.get('/api/users', (req, res) => {
+    Users.find()
+        .then(userz => {
+            res.status(200).json(userz)
+        })
+        .catch(err => {
+            console.log(err.message)
+        })
+})
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
